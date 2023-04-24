7682edfe41ad06bbb29f9732399b325d515ab760
minyoung10 <22000374@handong.ac.kr>
2023-04-24 17:12:56 +0900

    Update loadData Method

--- manager.c
+++ manager.c
@@ -56,8 +56,17 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
-
-
+	fp = fopen("product.txt","rt");
+        if(fp==NULL){
+                printf("=>파일 없음\n");
+                return 0;
+        }
+        for(int i=0; i<100; i++){
+                fscanf(fp,"%d %d %s",&p[i].weight,&p[i].price,p[i].name);
+                if(feof(fp))break;
+                count++;
+        }
+        fclose(fp);
 
 
 	printf("=> 로딩 성공!\n");
